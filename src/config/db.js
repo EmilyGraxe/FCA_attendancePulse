c3807589b265ef3f69b4833f2f8a4b9b75869ba9
emily <graxeemily@gmail.com>
2026-02-15 22:21:41 +0300

    prod db

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
 require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:  isProduction ? process.env.DATABASE_URL_PROD : process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
   max: 50,              // default 10, increase for heavy scanning
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
